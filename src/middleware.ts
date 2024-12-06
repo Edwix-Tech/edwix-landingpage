0d49405a6366ce5b6f3f5150adb59f377fee9a42
pierrecabriere <hello@pierrecabriere.fr>
2024-12-06 11:13:02 +0100

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(fr|en)/:path*'],
+  matcher: ['/((?!api|_next|_vercel\\..*).*)'],
 };
